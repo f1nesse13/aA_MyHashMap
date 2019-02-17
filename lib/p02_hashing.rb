b1b855ff4a10600846bdbec276a19312009a2f33
f1nesse13 <philly.teg@gmail.com>
2019-02-17 13:55:34 -0500

    hash function implemented

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,19 @@ end
 
 class Array
   def hash
-  end
+    sum = 0
+    self.each_with_index do |ele, i|
+      sum += ((sum << 8) + ele.to_s.ord + (sum >> 4))
+    end
+    return sum
+  end 
 end
 
 class String
   def hash
+    self.each_char.inject(0) do |sum, ele|
+      ((sum << 8) + ele.ord + (sum >> 4))
+    end
   end
 end
 
@@ -16,6 +24,7 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    arr = self.keys.sort
+    arr.hash
   end
 end
