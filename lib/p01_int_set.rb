4ac32b687f87d08c21a1da2fbc7dd2b1547ba5b4
f1nesse13 <philly.teg@gmail.com>
2019-02-16 20:36:09 -0500

    int_set implemented

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -1,19 +1,29 @@
 class MaxIntSet
   def initialize(max)
+    @store = Array.new(max, false)
   end
 
   def insert(num)
+    if is_valid?(num)
+      @store[num] = true
+    else
+      raise 'Out of bounds'
+    end
   end
 
   def remove(num)
+    @store[num] = false if is_valid?(num)
   end
 
   def include?(num)
+    @store[num] == true
   end
 
   private
 
   def is_valid?(num)
+    num.is_a?(Integer) &&
+    num.between?(0, @store.length)
   end
 
   def validate!(num)
@@ -27,19 +37,21 @@ class IntSet
   end
 
   def insert(num)
+    mod_ind = num % @store.length
+    @store[mod_ind] << num
   end
 
   def remove(num)
+    mod_ind = num % @store.length
+    @store[mod_ind].delete(num) if self.include?(num)
   end
 
   def include?(num)
+    mod_ind = num % @store.length
+    @store[mod_ind].include?(num)
   end
 
   private
-
-  def [](num)
-    # optional but useful; return the bucket corresponding to `num`
-  end
 
   def num_buckets
     @store.length
@@ -55,18 +67,33 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if !self[num].include?(num)
+      self[num] << num
+      @count += 1
+    end
+    if @count == num_buckets
+      @count -= @store.length / 2
+      resize!
+    end
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    bucket = num % num_buckets
+    @store[bucket]
   end
 
   def num_buckets
@@ -74,5 +101,17 @@ class ResizingIntSet
   end
 
   def resize!
+    num_buckets.times do |n|
+      @store << Array.new
+    end
+    re_insert!
+  end
+
+  def re_insert!
+    @store.each do |arr|
+      arr.each do |ele|
+        insert(ele)
+      end
+    end
   end
 end
