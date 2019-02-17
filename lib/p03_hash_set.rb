e0c64025004c5dd76726ee82ce08cc232ca644bc
f1nesse13 <philly.teg@gmail.com>
2019-02-17 18:34:24 -0500

    implemented hashset

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -7,18 +7,37 @@ class HashSet
   end
 
   def insert(key)
+    if @count == @store.length
+      resize!
+    end
+    if !self[key].include?(key)
+      self[key].push(key)
+      @count += 1
+    end
+  end
+
+  def re_insert(key)
+    self[key].push(key)
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if self[key].include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    hashed_key = num.hash
+    bucket = hashed_key % num_buckets
+    @store[bucket]
   end
 
   def num_buckets
@@ -26,5 +45,17 @@ class HashSet
   end
 
   def resize!
+    values = @store.map do |arr|
+      arr.each do |val|
+        val
+      end
+    end
+    num_buckets.times do |n|
+      @store << Array.new
+    end
+    values.each do |val|
+      self.re_insert(val)
+    end
   end
+
 end
